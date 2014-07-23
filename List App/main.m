2b1eff98ecd938d339f0e4ce6ef5585380808173
JaimeConnor <JaimeConnor@Jaimes-MacBook-Pro.local>
2014-07-23 10:15:18 -0400

    Initial Commit

---
+++ List App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  List App
+//
+//  Created by Jaime Connor on 7/23/14.
+//  Copyright (c) 2014 Jaime Connor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MFLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFLAppDelegate class]));
+    }
+}
